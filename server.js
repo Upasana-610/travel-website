9bde89d3bf2a94c1cb6235e14e6017834ce5e3e0
Roshni <sparksroshni610@gmail.com>
2023-08-25 22:03:18 +0530

    config

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+console.log(process.env.DATABASE);
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
